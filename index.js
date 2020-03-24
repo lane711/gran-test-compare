66d88a38f63c4e10da55dfb1ebec02372646f7ed
Lane Campbell <ldc0618@gmail.com>
2020-03-24 09:06:43 -0700

    update

--- index.js
+++ index.js
@@ -1,20 +1,44 @@
-let Parser = require('rss-parser');
-let parser = new Parser();
-fs = require('fs');
-const url = "http://defense.gov/DesktopModules/ArticleCS/RSS.ashx?ContentType=400&Site=945&max=20&isgovdelivery=1";
+const { promisify } = require("util");
+const fs = require("fs");
+const writeFile = promisify(fs.writeFile);
+const request = require("request");
+const url =
+  "https://defense.gov/DesktopModules/ArticleCS/RSS.ashx?ContentType=400&Site=945&max=20&isgovdelivery=1";
 
-(async () => {
- 
-  let feed = await parser.parseURL(url);
-  console.log(feed);
+const getGoogleIndexHTML = () => {
+  return new Promise((resolve, reject) => {
+    request(url, (err, res, body) => (err ? reject(err) : resolve(body)));
+  });
+};
 
-  fs.writeFile('files/1.txt', feed, function (err) {
-    if (err) return console.log(err);
-    console.log('Hello World > helloworld.txt');
-  });
- 
-  feed.items.forEach(item => {
-    // console.log(item.title + ':' + item.link)
-  });
- 
-})();
+const printAndWriteGoogleIndex = async () => {
+  try {
+    let googleIndexHTML = await getGoogleIndexHTML();
+    console.log(googleIndexHTML);
+
+    let fileName = generateFileName();
+    let filePath = `${__dirname}/files/${fileName}`;
+    await writeFile(filePath, googleIndexHTML, "utf8");
+    console.log("google-index.html written.");
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+printAndWriteGoogleIndex();
+
+// (async () => {
+
+//   let fileName = generateFileName();
+//   let filePath = `${__dirname}/files/${fileName}`;
+
+//   // fs.writeFile(filePath, feed.toString(), function(err) {
+//   //   if (err) return console.log(err);
+//   // });
+
+// })();
+
+function generateFileName() {
+  let now = new Date();
+  return now + ".txt";
+}
