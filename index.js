cff03c0d45c964dbae68b55d062173c31f34bab0
Lane Campbell <ldc0618@gmail.com>
2020-03-25 11:26:20 -0700

    file updates

--- index.js
+++ index.js
@@ -2,12 +2,11 @@ const { promisify } = require("util");
 const fs = require("fs");
 const writeFile = promisify(fs.writeFile);
 const request = require("request");
-require('colors');
-const url =
-  "http://10.211.55.3/DesktopModules/ArticleCS/RSS.ashx?ContentType=400&Site=2038&max=20&isgovdelivery=1";
-// "https://defense.gov/DesktopModules/ArticleCS/RSS.ashx?ContentType=400&Site=945&max=20&isgovdelivery=1";
+require("colors");
+// const url = "http://10.211.55.3/DesktopModules/ArticleCS/RSS.ashx?ContentType=400&Site=2038&max=20&isgovdelivery=1";
+const url = "https://defense.gov/DesktopModules/ArticleCS/RSS.ashx?ContentType=400&Site=945&max=20&isgovdelivery=1";
 var dateFormat = require("dateformat");
-var jsdiff = require('diff');
+var jsdiff = require("diff");
 var lastFile = "";
 var lastFileContent = "";
 
@@ -25,7 +24,7 @@ const printAndWriteGoogleIndex = async () => {
     let fileName = generateFileName();
     let filePath = `${__dirname}/files/${fileName}`;
     await writeFile(filePath, fileContent, "utf8");
-    console.log(`${fileName} written`);
+    // console.log(`${fileName} written`);
     compareFeeds(fileName, fileContent, lastFile, lastFileContent);
     lastFile = fileName;
     lastFileContent = fileContent;
@@ -36,7 +35,7 @@ const printAndWriteGoogleIndex = async () => {
 
 time = setInterval(function() {
   printAndWriteGoogleIndex();
-}, 1 * 10 * 1000); //5 minutes
+}, 1 * 60 * 1000); //1 minutes
 
 function generateFileName() {
   var now = new Date();
@@ -50,12 +49,17 @@ function compareFeeds(fileName, fileContent, lastFile, lastFileContent) {
     console.log(`comparing: ${fileName}<-->${lastFile}`);
 
     var diff = jsdiff.diffChars(fileContent, lastFileContent);
-    diff.forEach(function(part){
+    diff.forEach(function(part) {
       // green for additions, red for deletions
       // grey for common parts
-      var color = part.added ? 'green' :
-        part.removed ? 'red' : 'grey';
-      process.stderr.write(part.value[color]);
+      var color = part.added ? "green" : part.removed ? "red" : "grey";
+      if (color !== "grey") {
+        if (part.value.length > 6) {
+          process.stderr.write(part.value[color]);
+        } else {
+          // process.stderr.write("no major change\n");
+        }
+      }
     });
   }
 }
